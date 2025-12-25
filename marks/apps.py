02940e917de5410de912c22a5439b24e1a78f61d
MaksimShmakov <mighty.klaxon@yandex.ru>
2025-12-25 17:50:55 +0300

    fix

--- marks/apps.py
+++ marks/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
-from django.contrib.auth.models import Group
 
 
 def create_default_groups(sender, **kwargs):
     groups = ["Администратор", "Маркетолог", "Аналитик"]
+    from django.contrib.auth.models import Group
     for group_name in groups:
         Group.objects.get_or_create(name=group_name)
 
